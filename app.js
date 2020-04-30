d717170a61b7ece043665d30417da8585cb16f2b
Pablo Andres Diaz Mora <pabloandresp11@hotmail.com>
2020-04-30 18:40:03 -0500

    se realizo cambios para parcial

--- app.js
+++ app.js
@@ -22,16 +22,14 @@ document.getElementById("myStats").appendChild(stats.domElement);
 
 var geometry = new THREE.BoxGeometry( 1000, 1, 1000);
 var material = new THREE.MeshBasicMaterial( {color: 0xebebeb, side: THREE.DoubleSide} );
-var plane = new THREE.Mesh( geometry, material );
-scene.add( plane );
 
 // Create a basic perspective camera
 var camera = new THREE.PerspectiveCamera( 120, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 25;
+
+
+camera.position.z = 50;
 camera.position.y = 10;
-camera.position.x = 10
-
-
+camera.position.x = 5
 
 
 // Create a renderer with Antialiasing
@@ -47,33 +45,52 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 
+function createCilinder() {
+    let geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+    let material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+    let cylinder = new THREE.Mesh( geometry, material );
+    scene.add(cylinder);
+    cylinder.position.z -= 0
+}
+
+function createCube() {
+    let geometry = new THREE.BoxGeometry( 6, 6, 6 );
+    let material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+    let cube = new THREE.Mesh( geometry, material );
+    
+    cube.position.z += 30,
+    scene.add( cube );
+}
+
+function createToroide() {
+    let geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+    let material = new THREE.MeshBasicMaterial( { color: 0x0000FF } );
+    let torus = new THREE.Mesh( geometry, material );
+    
+    torus.position.z -= 30;
+    scene.add( torus );
+
+
+}
+
 // Render Loop
+let t = 0;
 var render = function () {
   stats.begin();
   requestAnimationFrame( render );
   var speed = Date.now() * 0.00025;
   
+  //mover hacia adelenate
+  camera.position.z -= 0.2;
+
+  //rotar
+  camera.position.x = 20*Math.cos(t) + 0;
+  t += 0.025;
   stats.end();
   renderer.render(scene, camera);
 };
 
+createCilinder();
+createCube();
+createToroide();
 render();
-
-window.onload = init;
-
-  function init(){
-    var botonAgregar = document.getElementById('btn-agregar').addEventListener("click", addFunction);
-  }
-
-  function addFunction() {
-
-    let randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-    console.log('click');
-
-    let geometry = new THREE.BoxGeometry( 1 , Math.random() * (15 -1) + 1, 1 );
-    let material = new THREE.MeshBasicMaterial( {color: randomColor});
-    let cube =  new THREE.Mesh( geometry, material );
-    cube.position.x = Math.random() * (25 ) + 1;
-    cube.position.z = Math.random() * (25 ) + 1;
-    scene.add(cube)
-  }
